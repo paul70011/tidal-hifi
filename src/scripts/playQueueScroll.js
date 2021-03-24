063bc5199b076e825e5c08e9ae9260e61972ffbf
Paul Steinebach <paul70011@live.de>
2021-03-24 11:44:54 +0100

    playQueueScroll: Changed button icon

--- src/scripts/playQueueScroll.js
+++ src/scripts/playQueueScroll.js
@@ -31,8 +31,7 @@ function createToCurrentButton(){
     const tooltipClasses = [...saveQueueWrapper.querySelector("span").classList]
 
     const toCurrentButton = document.createElement("button")
-    const playIcon = document.querySelector("[data-type='button__pause'] svg").cloneNode(true)
-    toCurrentButton.appendChild(playIcon)
+    toCurrentButton.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-test="icon-PlaybackPlay" class="elemental__icon"><path d="M5 2.9V21.1C5 21.9 5.9 22.4 6.55 21.9L19.8 12.8C20.4 12.4 20.4 11.55 19.8 11.15L6.55 2.1C5.9 1.6 5 2.1 5 2.9Z" stroke="#FCFCFC" stroke-width="2px"></path></svg>'
     toCurrentButton.classList.add(...buttonClasses)
     toCurrentButton.addEventListener("click", () => {
         scrollToCurrentPlayQueueItem()
